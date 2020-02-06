5c8c9f8b8ba289bb223dc1b558ec12d8b885c0c4
shmmmubtasim <shmmmubtasim>
2020-02-06 14:10:53 -0600

    Feb 6 Classwork

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -1,6 +1,6 @@
-// Project Title
-// Your Name
-// Date
+// Interactive Story
+// Maheer Mubtasim
+// February 6, 2020
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -9,7 +9,16 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
-
 function draw() {
-  background(220);
+  background(100);
+  character(mouseX,mouseY);
 }
+function character (x,y){
+  fill(255);
+  ellipse(x, y, 55, 55);
+  fill(14,11,2);
+  ellipse(x+10,y-5,10,10);
+  ellipse(x-10,y-5, 10,10);
+}
+  
+
