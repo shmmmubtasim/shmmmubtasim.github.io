1e7f050310cee10d9c748bc99294144885958cb6
shmmmubtasim <shmmmubtasim>
2020-02-07 14:19:17 -0600

    Update sketch.js

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -4,21 +4,30 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-
+let smileAnchor = 45;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 function draw() {
   background(100);
   character(mouseX,mouseY);
+  smile(mouseX,mouseY);
 }
 function character (x,y){
   fill(255);
-  ellipse(x, y, 55, 55);
-  fill(14,11,2);
-  ellipse(x+10,y-5,10,10);
-  ellipse(x-10,y-5, 10,10);
+  rect(x, y,60,60,15,15,40,40);
+  fill(0);
+  ellipse(x+17.5,y+17.5,10,10);
+  ellipse(x+42.5,y+17.5, 10,10);
+  ellipse(x+30,y+30,5,5);
+  fill(0);
+  line(x+30, y+60, x+30, y+145);
+  line(x,y+90, x+30, y+70);
+  line(x+30, y+70, x+60, y+90);
+  line(x,y+185, x+30, y+145);
+  line(x+30, y+145, x+60, y+185);
 }
-  
+function smile(x,y){
+  bezier(x+15, y+40, x+20,y+smileAnchor, x+40,y+smileAnchor, x+45, y+40);
+}
 
