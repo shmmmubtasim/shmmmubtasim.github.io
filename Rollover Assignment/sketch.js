72ea011da489503749e074b7b86ffb232c3c5edb
Mubtasim3802687 <3802687@sps-student.sk.ca>
2020-02-12 14:10:34 -0600

    Update sketch.js

--- Rollover Assignment/sketch.js
+++ Rollover Assignment/sketch.js
@@ -1,15 +1,31 @@
-// Project Title
-// Your Name
-// Date
+// State Variable 
+// Maheer Mubtasim
+// Feb 12, 2020
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800,800);
 }
 
 function draw() {
   background(220);
+  line(400,0,400,800);
+  line(0,400,800,400);
+}
+function quadrant(){
+  if (mouseX <= 400 && mouseY <= 400){
+    return 2;
+  } 
+  else if (mouseX >= 400 && mouseY <= 400){
+    return 1;
+  } 
+  else if (mouseX <= 400 && mouseY <= 400){
+    return 2;
+  } 
+  else if (mouseX <= 400 && mouseY <= 400){
+    return 2;
+  }
 }
