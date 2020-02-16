b40e6b413e79bd64f1e67f2ff9d0d67dc107b995
shmmmubtasim <60625573+shmmmubtasim@users.noreply.github.com>
2020-02-16 08:50:32 -0600

    Update sketch.js

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -4,31 +4,15 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let horizon;
-let currentBack = 0;
 let smileAnchor = 45;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  horizon = windowHeight * 0.8;
 }
 function draw() {
-  backgroundImage();
-  
-  
-  strokeWeight(5);
-  if (mouseY < horizon-185){
-    character(mouseX,mouseY);
-    smile(mouseX,mouseY);
-    smileChange();
-  }
-  else {
-    character(mouseX,horizon-185);
-    smile(mouseX,horizon-185);
-    smileChange();
-  }
-  textSize(50);
-  text("Maheer", windowWidth - 200, windowHeight-50);
+  background(100);
+  character(mouseX,mouseY);
+  smile(mouseX,mouseY);
+  smileChange();
 }
 function character (x,y){
   fill(255);
@@ -50,64 +34,10 @@ function smile(x,y){
 function smileChange(){
   if (keyIsPressed === true){
     if (keyCode === ENTER){
-      smileAnchor = smileAnchor + 1;
+      smileAnchor = smileAnchor + 0.2;
     }
     if (keyCode === BACKSPACE){
-      smileAnchor = smileAnchor - 1;
-    }
-  }
-}
-function valley(){
-  fill(106,190,69);
-  rect(0,horizon,windowWidth,windowHeight-horizon);
-  fill(19,139,67);
-  triangle(200, horizon, 325, horizon-800, 400, horizon);
-  triangle(50, horizon, 200, horizon-700, 500, horizon);
-  triangle(200, horizon, 400, horizon-500, 600, horizon);
-}
-function clouds(x,y,size){
-  strokeWeight(0);
-  fill(255);
-  ellipse(x,y-size/1.5,size,size);
-  ellipse(x+size/3,y-size/3,size,size);
-  ellipse(x+size/1.5,y,size,size);
-  ellipse(x+size/3,y+size/3,size,size);
-  ellipse(x,y+size/1.5,size,size);
-  ellipse(x-size/3,y+size/3,size,size);
-  ellipse(x-size/1.5,y,size,size);
-  ellipse(x-size/3,y-size/3,size,size);
-  ellipse(x,y,size,size);
-  ellipse(x,y,size,size);
-}
-function backgroundImage(){
-  if (currentBack === 0){ //day
-    background(51,153,255);
-    valley();
-    clouds();
-  }
-  if (currentBack === 1){ //night
-    background(51,153,255);
-    valley();
-    clouds();
-  }
-  if (currentBack === 2){ //cloudy day
-    background(51,153,255);
-    valley();
-    clouds();
-  }
-  if (currentBack === 3){ //cloudy night
-    background(51,153,255);
-    valley();
-    clouds();
-  }
-}
-function timeOfDay(){
-  if (keyIsPressed === true){
-    if (keyCode === ENTER){
-      smileAnchor = smileAnchor + 1;
-    }
-    if (keyCode === BACKSPACE){
-      smileAnchor = smileAnchor - 1;
+      smileAnchor = smileAnchor - 0.2;
     }
   }
 }
