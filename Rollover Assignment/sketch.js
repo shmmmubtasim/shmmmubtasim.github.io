493b4e1879dc0b540ea6e7f68be8f7fec558ed68
shmmmubtasim <60625573+shmmmubtasim@users.noreply.github.com>
2020-02-22 03:35:24 -0600

    Update sketch.js

--- Rollover Assignment/sketch.js
+++ Rollover Assignment/sketch.js
@@ -11,11 +11,31 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(255);
+  strokeWeight(8);
+  fill(255);
+  rect(0,0,800,800)
   findQuadrant();
+  strokeWeight(3);
   line(400,0,400,800);
   line(0,400,800,400);
-  
+  print(quadrant); 
+  if (quadrant === 1){
+    fill(0);
+    rect(400,0,400,400);
+  } 
+  if (quadrant === 2){
+    fill(0);
+    rect(0,0,400,400);
+  } 
+  if (quadrant === 3){
+    fill(0);
+    rect(0,400,400,400);
+  } 
+  if (quadrant === 4){
+    fill(0);
+    rect(400,400,400,400);
+  } 
 }
 function findQuadrant(){
   if (mouseX <= 400 && mouseY <= 400){
@@ -31,3 +51,4 @@ function findQuadrant(){
     quadrant = 4;
   }
 }
+
