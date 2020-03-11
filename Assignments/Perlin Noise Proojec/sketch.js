d981e948fc3ad560e066207cac9a60859e02b0f7
shmmmubtasim <60625573+shmmmubtasim@users.noreply.github.com>
2020-03-11 00:44:55 -0600

    Update sketch.js

--- Assignments/Perlin Noise Proojec/sketch.js
+++ Assignments/Perlin Noise Proojec/sketch.js
@@ -6,19 +6,26 @@
 // - describe what you did to take this project "above and beyond"
 
 let rectHeight, rectWidth, maxHeight;
+let position = 500;
+let thiccness = 1000;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
-  
-  rectWidth = width/5000;
-  rectHeight = height/2,height;
-  for (x = 0;x < width; x += rectWidth){
-    rectHeight += random(noise(rectHeight / 1000),noise(rectHeight / -1000)); 
-     
-    rect(x,height-rectHeight,rectWidth,rectHeight);
-  }
 }
 
 
-function draw() {}
+function draw() {
+  background(220);
+  generateTerrain();
+}
+
+function generateTerrain (){
+  rectWidth = width/thiccness;
+  rectHeight = height/3;
+  for (x = 0;x < width; x += rectWidth){
+    rectHeight = noise((position+x) * 0.004);
+    rect(x,height*rectHeight,rectWidth,height+rectHeight);
+    position = position + 0.005;
+  }
+}
