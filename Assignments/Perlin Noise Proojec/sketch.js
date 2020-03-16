c7693979aa5b7ff7a8c783a1a7c5795db7830d0a
Mubtasim3802687 <3802687@sps-student.sk.ca>
2020-03-16 14:04:32 -0600

    Update sketch.js

--- Assignments/Perlin Noise Proojec/sketch.js
+++ Assignments/Perlin Noise Proojec/sketch.js
@@ -1,11 +1,12 @@
-// Project Title
-// Your Name
-// Date
+// Perlin Noise Project
+// Maheer Mubtasim
+// March 13, 2020
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - the sketch pans
+// - a flag is drawn at the highest point
 
-let rectHeight, rectWidth, maxHeight;
+let rectHeight, rectWidth,tallest,tallestX;
 let position = 500;
 let thiccness = 1000;
 
@@ -18,14 +19,26 @@ function setup() {
 function draw() {
   background(220);
   generateTerrain();
+  drawFlag();
 }
 
-function generateTerrain (){
-  rectWidth = width/thiccness;
-  rectHeight = height/3;
-  for (x = 0;x < width; x += rectWidth){
+function generateTerrain (){ //function to execute the code for drawing all the rectangles that make up the terrain
+  rectWidth = width/thiccness; 
+  rectHeight = height/3; 
+  tallest = height;
+  tallestX = 0;
+  for (let x = 0;x < width; x += rectWidth){ //until the whole screen is filled, draw these rectangles
     rectHeight = noise((position+x) * 0.004);
     rect(x,height*rectHeight,rectWidth,height+rectHeight);
     position = position + 0.005;
+    if (rectHeight*height < tallest){ //determines which rectangle stands the tallest and where along the x-axis is it located
+      tallest = rectHeight*height;
+      tallestX = x;
+    }
   }
+}
+function drawFlag() { //draws a flag at the highest point
+  strokeWeight(5);
+  line(tallestX, tallest-50,tallestX,tallest);
+  triangle(tallestX, tallest-50, tallestX+20,tallest-40, tallestX,tallest-30);
 }
